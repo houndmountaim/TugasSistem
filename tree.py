eb056f99ff2bdf544a2c3a20024f07c6c46097f3
houndmountaim <157226724+houndmountaim@users.noreply.github.com>
2024-01-22 10:41:43 +0700

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,38 @@
+class Node :
+    def __init__(self, abc) :
+        self.right = None
+        self.left = None
+        self.data = abc
+        
+        #right > parent
+        #left < parent
+        
+    def insert (self, abc):
+        if self.data:
+            if abc < self.data:
+                if self.left is None:
+                    self.left = Node(abc)
+                else:
+                    self.left.insert(abc)
+            elif abc > self.data:
+                if self.right is None:
+                    self.right = Node(abc)
+                else:
+                    self.right.insert(abc)
+        else:
+            self.data = abc 
+    
+    def cetak (self):
+        if self.left:
+            self.left.cetak()
+        print(self.data)
+        if self.right:
+            self.right.cetak()
+            
+root = Node(10)
+root.insert (3)
+root.insert (4)
+root.insert (6)
+root.insert (8)
+root.insert (12)
+root.cetak () 
